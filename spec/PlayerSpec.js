e886633634d284411708508ac1e9a9431f2536e4
elenamorton <elena.uleia@gmail.com>
2017-07-14 16:23:41 +0100

    added first test for player name

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -5,5 +5,9 @@ describe("Player", function() {
     player = new Player();
   });
 
+  it('can keep the plaayer identity as X or O', function() {
+    expect((player.getName() == 'X') || (player.getName() == 'O')).toBe(true);
+  });
+
 
 });
